1ce3159db0314ca36f2f94294dcad1241eb400ae
Your Name <you@example.com>
2021-12-03 12:11:43 +0000

    adds uk parliament datasource

---
+++ test/geo/test_parliament_api.py
@@ -0,0 +1,28 @@
+from test.tags import integration_test
+
+from django.test import TestCase
+
+from pyck.geo.territories.uk import parliament
+
+
+@integration_test
+class ParliamentApiTests(TestCase):
+    def test_returns_constituencies(self):
+        self.assertListReturnsAtLeastCount(parliament.constituencies, 300)
+        self.assertCanGetResourceReturnedFromList(parliament.constituencies)
+
+    def test_returns_members(self):
+        self.assertListReturnsAtLeastCount(parliament.mps, 300)
+        self.assertCanGetResourceReturnedFromList(parliament.mps)
+
+    def test_returns_parties(self):
+        self.assertListReturnsAtLeastCount(parliament.parties, 4)
+        self.assertCanGetResourceReturnedFromList(parliament.parties)
+
+    def assertListReturnsAtLeastCount(self, resource_type, expected):
+        results = list(resource_type.list())
+        self.assertGreater(len(results), expected)
+
+    def assertCanGetResourceReturnedFromList(self, resource_type):
+        resource = next(resource_type.list())
+        resource_type.get(resource_type.get_id(resource))
